439cd2b6b22457551e20eb2176440265d745ce3e
cmolinan <cmolinan@gmail.com>
2022-07-28 16:18:36 -0500

    Finishing the mobile version

--- code.js
+++ code.js
@@ -1,11 +1,14 @@
 const queryMobileMenu = document.getElementById('MobileMenu');
 const queryMobile_Nav = document.getElementById('mobile-nav');
+const queryMain = document.body;
 
 function MobileMenu() {
   if ( queryMobileMenu.style.display == 'flex') {
     queryMobileMenu.style.display = 'none';
+    queryMain.style.overflow = 'visible';
     // queryMobile_Nav.style.display = 'block';
   } else {
+    queryMain.style.overflow = 'hidden';
     queryMobileMenu.style.display  = 'flex';
     // queryMobile_Nav.style.display = 'none';
     }
